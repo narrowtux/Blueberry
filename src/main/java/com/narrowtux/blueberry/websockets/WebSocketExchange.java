a9646b02da4bba2605d336aec2b35ad33a97a973
narrowtux <narrow.m@gmail.com>
2012-10-10 17:40:31 +0200

    Fixed crash when sending a frame to a broken stream

--- src/main/java/com/narrowtux/blueberry/websockets/WebSocketExchange.java
+++ src/main/java/com/narrowtux/blueberry/websockets/WebSocketExchange.java
@@ -3,9 +3,9 @@ package com.narrowtux.blueberry.websockets;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.SocketException;
 
 import com.narrowtux.blueberry.http.HttpExchange;
-import com.narrowtux.blueberry.http.headers.HttpHeaders;
 
 public class WebSocketExchange {
 	private InputStream in;
@@ -44,39 +44,45 @@ public class WebSocketExchange {
 	}
 	
 	public void sendFrame(Frame frame) throws IOException {
-		boolean fin = true;
-		byte opcode = (byte) frame.getOpCode();
-		boolean maskEnabled = false;
-		byte data[] = frame.getPayload();
-		long len = data != null ? data.length : 0;
-		
-		byte firstlen = (byte) len;
-		if (len > 125) {
-			firstlen = 126;
-		}
-		if (len > 65535) {
-			firstlen = 127;
-		}
-		
-		out.write((fin ? 0x80 : 0x00)| opcode);
-		out.write((maskEnabled ? 0x80 : 0x00) << 7 | firstlen);
-		
-		if (firstlen == 126) {
-			out.write((int) (len >> 8));
-			out.write((int) len);
-		} else if (firstlen == 127) {
-			out.write((int) (len >> 56));
-			out.write((int) (len >> 48));
-			out.write((int) (len >> 40));
-			out.write((int) (len >> 32));
-			out.write((int) (len >> 24));
-			out.write((int) (len >> 16));
-			out.write((int) (len >> 8));
-			out.write((int) len);
-		}
-		
-		if (data != null) {
-			out.write(data);
+		try {
+			boolean fin = true;
+			byte opcode = (byte) frame.getOpCode();
+			boolean maskEnabled = false;
+			byte data[] = frame.getPayload();
+			long len = data != null ? data.length : 0;
+			
+			byte firstlen = (byte) len;
+			if (len > 125) {
+				firstlen = 126;
+			}
+			if (len > 65535) {
+				firstlen = 127;
+			}
+			
+			out.write((fin ? 0x80 : 0x00)| opcode);
+			out.write((maskEnabled ? 0x80 : 0x00) << 7 | firstlen);
+			
+			if (firstlen == 126) {
+				out.write((int) (len >> 8));
+				out.write((int) len);
+			} else if (firstlen == 127) {
+				out.write((int) (len >> 56));
+				out.write((int) (len >> 48));
+				out.write((int) (len >> 40));
+				out.write((int) (len >> 32));
+				out.write((int) (len >> 24));
+				out.write((int) (len >> 16));
+				out.write((int) (len >> 8));
+				out.write((int) len);
+			}
+			
+			if (data != null) {
+				out.write(data);
+			}
+		} catch (SocketException e) {
+			if (open) {
+				close();
+			}
 		}
 	}
 }
